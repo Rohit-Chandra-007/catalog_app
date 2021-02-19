5ec493d2e18fa806ed3b08d38580d6ff67521f0e
Rohit Chandra <chandrarohit91@gmail.com>
2021-02-19 10:48:39 +0530

    flutter run --release

---
+++ lib/widgets/catalog_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_one/modals/catalog.dart';
+import 'package:flutter_app_one/widgets/my_app_theme.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+
+class CatalogWidget extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogWidget({Key key, @required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+      child: Row(
+        children: [
+          CatalogImage(
+            image: catalog.image,
+          ),
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  catalog.name.text.lg.color(MyAppTheme.darkBluishColor).bold.make(),
+                  catalog.desc.text.textStyle(context.captionStyle).make(),
+                  10.heightBox,
+                  ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    buttonPadding: EdgeInsets.zero,
+                    children: [
+                      "\$${catalog.price}".text.bold.xl.make(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              MyAppTheme.darkBluishColor,
+                            ),
+                            shape: MaterialStateProperty.all(
+                              StadiumBorder(),
+                            )),
+                        child: "Buy".text.make(),
+                      )
+                    ],
+                  ).pOnly(right: 8.0)
+                ],
+              ))
+        ],
+      ),
+    ).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key key, @required this.image}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+    ).box.rounded.p8.color(Colors.white).make().p16().w40(context);
+  }
+}
